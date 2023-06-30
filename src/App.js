13d62b8c931fe7c9bb17b35c86b74510e933c3a5
dsouza <dhesouzanorbert36@gmail.com>
2023-06-30 17:17:17 +0300

    Added the random quotes functinality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect} from "react";
+import twitter from "./assets/twit.svg";
+import tumblr from "./assets/tumblr.svg";
+
+
 
 function App() {
+
+  // creating state to store the quotes and authors
+  const [quoteData, setQuateData] = useState({});
+
+  // using useEffect to fetch the data from the API
+  useEffect(() => {
+    quoteGenerator();
+  },[])
+  // console.log(quoteData);
+ 
+  // Quote generator
+const quoteGenerator = () => {
+  fetch("https://api.quotable.io/random")
+  .then(res => res.json())
+  .then(data => setQuateData({
+      text: data.content,
+      author: data.author
+  }));
+
+   // colors 
+   let colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+}
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <div id="quote-box">
+        {/* quote container */}
+        <div id="text">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-quote" viewBox="0 0 16 16">
+          <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"/>
+        </svg>
+        <i class="fa-solid fa-quote-left"></i>
+          <span>{quoteData.text}</span>
+        </div>
+
+        {/* The quote author */}
+        <div  id="author">
+        <span> - {quoteData.author}</span>
+        </div>
+
+        {/* social media links */}
+        <div>
+          {/* twitter */}
+          <a href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22We%20can%20easily%20forgive%20a%20child%20who%20is%20afraid%20of%20the%20dark%3B%20the%20real%20tragedy%20of%20life%20is%20when%20men%20are%20afraid%20of%20the%20light.%22%20Plato" id="tweet-quote">
+            <img src={twitter} alt="twitter icon" className="social-icons"/>
+          </a>
+          {/* tumblr */}
+          <a href="https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Plato&content=We%20can%20easily%20forgive%20a%20child%20who%20is%20afraid%20of%20the%20dark%3B%20the%20real%20tragedy%20of%20life%20is%20when%20men%20are%20afraid%20of%20the%20light.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button">
+            <img src={tumblr} alt="tumblr icon" className="social-icons"/>
+          </a>
+                 
+            {/* quote generator */}
+          <button id="new-quote" onClick={quoteGenerator}>New quote</button>
+        </div>
+
+      </div>
     </div>
   );
 }
